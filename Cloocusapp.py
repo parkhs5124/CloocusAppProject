db7802ee97e1d45c899b397e43906b110bf69bf4
parkhs5124 <parkhs5124@naver.com>
2019-07-05 04:43:14 +0000

    Cloocusapp.dockerfile

---
+++ Cloocusapp.py
@@ -0,0 +1,8 @@
+import platform
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+return "Hello, Cloocus. I'm in {}".format(platform.node())
+if __name__ == '__main__':
+app.run(host='0.0.0.0', port=8000)
